001f3aac76642d20bc49d7d4c01bbd2d0d9c6c57
Kira <kawoods826@gmail.com>
2019-09-30 12:20:04 -0700

    added functions to add new item and reset state

--- src/App.js
+++ src/App.js
@@ -10,10 +10,51 @@ class App extends Component {
       list: []
     }
   }
+
+  updateInput(key, value) {
+    //update react state
+    this.setState({
+      [key]: value
+    });
+  }
+
+  addItem() {
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+
+    //copy of current list of items
+    const list = [...this.state.list];
+
+    //add new item to list 
+    list.push(newItem);
+
+    //update state with new list and reset newItem input
+    this.setState({
+      newItem: "",
+      list
+    })
+  }
+
   render (){
     return (
       <div className="App">
-        <h1>To Do List</h1>
+        <div>
+          Add an item...
+          <br/>
+          <input
+          type = "text"
+          placeholder = "Type item here..."
+          value = {this.state.newItem}
+          onChange = {e => this.updateInput("newItem", e.target.value)}
+          />
+          <button 
+            onClick = {() => this.addItem}
+          >
+            Add
+          </button>
+        </div>
       </div>
     );
   }
